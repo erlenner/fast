2b63459a86f696d324cc7972c935db29855bb9d2
Erlend Eriksen <erlend_ne@hotmail.com>
2019-08-12 12:29:03 +0200

    add missing const

--- src/faster_corner_10_sse.cpp
+++ src/faster_corner_10_sse.cpp
@@ -14,7 +14,7 @@
 namespace fast
 {
   template <bool Aligned> void faster_corner_detect_10(const fast_byte* img, int img_width, int img_height, int img_stride,
-                                                       short barrier, std::vector<fast_xy>& corners)
+                                                       const short barrier, std::vector<fast_xy>& corners)
   {
       const int w = img_stride; //cfo: before: I.size().x;
       const int stride = 3*w;
